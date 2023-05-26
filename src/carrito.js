9acd353247c7ad0ee5d351d9f1dc03c5952e04ad
Nacho Jimenez <ignaciokaram@gmail.com>
2023-05-26 16:57:38 -0300

    agregando funcionalidad carrito y css

--- src/carrito.js
+++ src/carrito.js
@@ -1,52 +1,51 @@
+$(document).ready(function() {
+  // Carrito de compras
+  var cartItems = [];
+  var total = 0;
 
-  $(document).ready(function() {
-    // Carrito de compras
-    var cartItems = [];
-    var total = 0;
+  $('.add-to-cart-btn').click(function(e) {
+    e.preventDefault();
+    var productName = $(this).data('name');
+    var productPrice = parseFloat($(this).data('price'));
 
-    $('.add-to-cart-btn').click(function(e) {
-      e.preventDefault();
-      var productName = $(this).data('name');
-      var productPrice = parseFloat($(this).data('price'));
+    // Agregar el producto al carrito
+    cartItems.push({ name: productName, price: productPrice });
 
-      // Agregar el producto al carrito
-      cartItems.push({ name: productName, price: productPrice });
+    // Actualizar el carrito
+    updateCart();
+  });
 
-      // Actualizar el carrito
-      updateCart();
-    });
+  function updateCart() {
+    $('#cart-items').empty();
+    total = 0;
 
-    function updateCart() {
-      $('#cart-items').empty();
-      total = 0;
-
-      // Agregar los productos al carrito
-      for (var i = 0; i < cartItems.length; i++) {
-        var item = cartItems[i];
-        $('#cart-items').append('<li>' + item.name + ' - $' + item.price.toFixed(2) + '</li>');
-        total += item.price;
-      }
-
-      // Actualizar el total
-      $('#cart-total').text('$' + total.toFixed(2));
+    // Agregar los productos al carrito
+    for (var i = 0; i < cartItems.length; i++) {
+      var item = cartItems[i];
+      $('#cart-items').append('<li>' + item.name + ' - $' + item.price.toFixed(2) + '</li>');
+      total += item.price;
     }
 
-    // Proceder al pago (checkout)
-    $('#checkout-btn').click(function() {
-      // Aquí puedes agregar la lógica para el proceso de pago
-      alert('¡Gracias por tu compra!');
-    });
-  });
+    // Actualizar el total
+    $('#cart-total').text('$' + total.toFixed(2));
+  }
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var cartToggle = document.getElementById('cart-toggle');
-    var cartContainer = document.getElementById('cart-items');
-  
-    cartToggle.addEventListener('click', function() {
-      if (cartContainer.style.display === 'none') {
-        cartContainer.style.display = 'block';
-      } else {
-        cartContainer.style.display = 'none';
-      }
-    });
+  // Proceder al pago (checkout)
+  $('#checkout-btn').click(function() {
+    // Aquí puedes agregar la lógica para el proceso de pago
+    alert('¡Gracias por tu compra!');
   });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var cartToggle = document.getElementById('cart-toggle');
+  var cartContainer = document.getElementById('cart-items');
+
+  cartToggle.addEventListener('click', function() {
+    if (cartContainer.style.display === 'none') {
+      cartContainer.style.display = 'block';
+    } else {
+      cartContainer.style.display = 'none';
+    }
+  });
+});
