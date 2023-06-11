986cec0824d8269f1d5108f2a72ce2da00596091
Ignacio Jimenez <ignaciokaram@gmail.com>
2023-06-10 23:23:59 -0300

    ABM`s
    .Net framework C# en el cual desarrollamos la primer ABM modificación de perfil (lado del usuario)

---
+++ MedicarloSQL/Solution1/WebApplication1/App_Start/WebApiConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebApplication1
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            var cors = new EnableCorsAttribute("http://localhost:59323", "*", "GET,POST,PUT,DELETE");
+
+            config.EnableCors(cors);
+            // Configuración y servicios de API web
+
+            // Rutas de API web
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
+
---
+++ MedicarloSQL/Solution1/WebApplication1/Controllers/UsuariosController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class UsuariosController : ApiController
+    {
+        // GET: api/Usuarios
+        public List<Usuarios> Get()
+        {
+            List<Usuarios> oList = new List<Usuarios>();
+            using (TestEntities db = new TestEntities())
+            {
+                oList = db.Usuarios.ToList();
+            }
+            return oList;
+        }
+
+        // GET: api/Usuarios/id
+        public Usuarios Get(int id)
+        {
+            Usuarios oItem = new Usuarios();
+            using (TestEntities db = new TestEntities())
+            {
+                oItem = db.Usuarios.Find(id);
+            }
+            return oItem;
+        }
+
+        //POST: api/Alumno
+        public void Post([FromBody] Usuarios value)
+        {
+            using (TestEntities db = new TestEntities())
+            {
+                db.Usuarios.Add(value);
+                db.SaveChanges();
+            }
+        }
+
+        // PUT: api/Usuarios/5
+        public void Put(int id, [FromBody] Usuarios value)
+        {
+            using (TestEntities db = new TestEntities())
+            {
+                Usuarios oItem = db.Usuarios.Find(id);
+                if (oItem != null)
+                {
+                    oItem.Nombre = value.Nombre;
+                    oItem.Apellido = value.Apellido;
+                    oItem.Correo = value.Correo;
+                    oItem.Contraseña = value.Contraseña;
+                    db.Entry(oItem).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                }
+                else
+                {
+                    Console.WriteLine("Usuario no existente");
+                    // Manejar el caso en el que no se encuentre el objeto con el ID dado
+                    // Por ejemplo, puedes lanzar una excepción, mostrar un mensaje de error, etc.
+                }
+            }
+        }
+
+        // DELETE: api/Usuarios/5
+        public void Delete(int id)
+        {
+            using (TestEntities db = new TestEntities())
+            {
+                Usuarios oItem = db.Usuarios.Find(id);
+                db.Usuarios.Remove(oItem);
+                db.SaveChanges();
+            }
+        }
+    }
+}
